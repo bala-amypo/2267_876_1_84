453fcd97bb102e9d0cac70035fee7509b36de8ef
bala-amypo <bala@amypo.in>
2025-12-18 03:59:41 +0000

    User commit

---
+++ src/main/java/com/example/demo/model/Vehicle.java
@@ -0,0 +1,100 @@
+package com.example.demo.model;
+import jakarta.persistence.*;
+
+@Entity
+public class Vehicle{
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String vin;
+
+    private String make;
+    private String model;
+    private Integer year;
+
+    private Long ownerId;
+
+    private Boolean active = true; // default true
+
+    private Timestamp createdAt;
+
+    public Vehicle() {
+    }
+
+    public Vehicle(Long id, String vin, String make, String model,
+                   Integer year, Long ownerId, Boolean active, Timestamp createdAt) {
+        this.id = id;
+        this.vin = vin;
+        this.make = make;
+        this.model = model;
+        this.year = year;
+        this.ownerId = ownerId;
+        this.active = active;
+        this.createdAt = createdAt;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getVin() {
+        return vin;
+    }
+
+    public void setVin(String vin) {
+        this.vin = vin;
+    }
+
+    public String getMake() {
+        return make;
+    }
+
+    public void setMake(String make) {
+        this.make = make;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public Integer getYear() {
+        return year;
+    }
+
+    public void setYear(Integer year) {
+        this.year = year;
+    }
+
+    public Long getOwnerId() {
+        return ownerId;
+    }
+
+    public void setOwnerId(Long ownerId) {
+        this.ownerId = ownerId;
+    }
+
+    public Boolean getActive() {
+        return active;
+    }
+
+    public void setActive(Boolean active) {
+        this.active = active;
+    }
+
+    public Timestamp getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Timestamp createdAt) {
+        this.createdAt = createdAt;
+    }
+}
