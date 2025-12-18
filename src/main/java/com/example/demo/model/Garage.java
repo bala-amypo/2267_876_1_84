3a7894dca36a8fba43b90a3f6da44a93a69e8f6b
bala-amypo <bala@amypo.in>
2025-12-18 08:59:29 +0000

    User commit

---
+++ src/main/java/com/example/demo/model/Garage.java
@@ -0,0 +1,27 @@
+package com.example.demo.model;
+
+
+import jakarta.persistence.*;
+
+
+@Entity
+public class Garage {
+
+
+@Id
+@GeneratedValue(strategy = GenerationType.IDENTITY)
+private Long id;
+
+
+@Column(unique = true)
+private String garageName;
+private String address;
+private String contactNumber;
+private Boolean active = true;
+
+
+public Garage() {}
+public Long getId() { return id; }
+public Boolean getActive() { return active; }
+public void setActive(Boolean active) { this.active = active; }
+}
