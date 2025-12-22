5581861206a4febdb6ea0576f3af600f16f881c8
bala-amypo <bala@amypo.in>
2025-12-22 08:39:58 +0000

    User commit

--- src/main/java/com/example/demo/controller/VehicleController.java
+++ src/main/java/com/example/demo/controller/VehicleController.java
@@ -16,25 +16,20 @@ public class VehicleController {
         this.service = service;
     }
 
-    // POST – Register vehicle
     @PostMapping
     public Vehicle register(@RequestBody Vehicle vehicle) {
         return service.registerVehicle(vehicle);
     }
 
-    // GET – Vehicle by ID
     @GetMapping("/{id}")
     public Vehicle getById(@PathVariable Long id) {
         return service.getVehicleById(id);
     }
 
-    // GET – Vehicle by VIN
     @GetMapping("/vin/{vin}")
     public Vehicle getByVin(@PathVariable String vin) {
         return service.getVehicleByVin(vin);
     }
-
-    // GET – Vehicles by Owner
     @GetMapping("/owner/{ownerId}")
     public List<Vehicle> getByOwner(@PathVariable Long ownerId) {
         return service.getVehiclesByOwner(ownerId);
