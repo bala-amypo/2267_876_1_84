9875b0a8e248db4c295f53e84d3d00b02fe35bef
bala-amypo <bala@amypo.in>
2025-12-23 06:32:42 +0000

    User commit

--- src/main/java/com/example/demo/exception/EntityNotFoundException.java
+++ src/main/java/com/example/demo/exception/EntityNotFoundException.java
@@ -1,7 +1,8 @@
 package com.example.demo.exception;
 
 public class EntityNotFoundException extends RuntimeException {
-    public EntityNotFoundException(String msg) {
-        super(msg);
+
+    public EntityNotFoundException(String message) {
+        super(message);
     }
 }
