ab69aa9d62bc97b974288977df3b5efe25364967
bala-amypo <bala@amypo.in>
2025-12-25 10:56:12 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/VerificationLogServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/VerificationLogServiceImpl.java
@@ -6,10 +6,12 @@ import com.example.demo.model.VerificationLog;
 import com.example.demo.repository.ServiceEntryRepository;
 import com.example.demo.repository.VerificationLogRepository;
 import com.example.demo.service.VerificationLogService;
+import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
+@Service
 public class VerificationLogServiceImpl implements VerificationLogService {
 
     private final VerificationLogRepository verificationLogRepository;
