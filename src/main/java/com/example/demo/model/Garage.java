fe156c40e1b8635620b8d5d14eaaf1a7c6e75946
bala-amypo <bala@amypo.in>
2025-12-26 03:39:23 +0000

    User commit

--- src/main/java/com/example/demo/model/Garage.java
+++ src/main/java/com/example/demo/model/Garage.java
@@ -4,8 +4,8 @@ import jakarta.persistence.*;
 
 @Entity
 @Table(
-    name = "garages",
-    uniqueConstraints = @UniqueConstraint(columnNames = "garageName")
+        name = "garages",
+        uniqueConstraints = @UniqueConstraint(columnNames = "garage_name")
 )
 public class Garage {
 
@@ -13,21 +13,21 @@ public class Garage {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(nullable = false, unique = true)
+    @Column(name = "garage_name", nullable = false, unique = true)
     private String garageName;
 
+    @Column(nullable = false)
     private String address;
+
+    @Column(nullable = false)
     private String contactNumber;
 
+    @Column(nullable = false)
     private Boolean active = true;
 
     // ===== Getters & Setters =====
     public Long getId() {
         return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
     }
 
     public String getGarageName() {
